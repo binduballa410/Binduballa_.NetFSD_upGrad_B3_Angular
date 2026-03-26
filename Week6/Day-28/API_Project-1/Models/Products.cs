873b73083228e88dfa31088bf9b36409acd86050
binduballa410 <binduballa410@gmail.com>
2026-03-26 19:53:12 +0530

    Add files via upload

---
+++ Week6/Day-28/API_Project-1/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public string ProName { get; set; }
+        public int Price { get; set; }
+
+    }
+}
