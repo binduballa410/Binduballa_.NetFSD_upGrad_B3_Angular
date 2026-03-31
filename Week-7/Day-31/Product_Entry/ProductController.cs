8d10d10a37c0485bc9042b0b3a2ff488b69917a5
binduballa410 <binduballa410@gmail.com>
2026-03-31 20:28:38 +0530

    Add files via upload

---
+++ Week-7/Day-31/Product_Entry/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Route("Product")]
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>();
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            ViewBag.Products = products;
+            return View();
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            if (product != null)
+            {
+                products.Add(product);
+            }
+            ViewBag.Products = products;
+            return View("Index");
+        }
+
+    }
+}
