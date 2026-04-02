3048f99038d8a310149f51b0fb496b52cd24910e
binduballa410 <binduballa410@gmail.com>
2026-04-02 11:18:56 +0530

    Add files via upload

---
+++ Week-7/Day-31/Day-30/ContactInfo/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        public static List<ContactInfo> contacts = new List<ContactInfo>();
+        public ActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+        public ActionResult GetContactId(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+            return View(contact);
+        }
+        public ActionResult AddContact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddContact(ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
+
